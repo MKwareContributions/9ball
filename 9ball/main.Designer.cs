1e126d7d67439db74c295caa6e2b11e260c76468
MKware <mrkodomoh64@hotmail.com>
2022-03-07 00:20:49 +0300

    Work in progress

---
+++ 9ball/main.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace _9ball
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.minButton = new System.Windows.Forms.PictureBox();
+            this.closeButton = new System.Windows.Forms.PictureBox();
+            this.title = new System.Windows.Forms.Label();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.shake = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.minButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.closeButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.title);
+            this.panel1.Controls.Add(this.minButton);
+            this.panel1.Controls.Add(this.closeButton);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(676, 30);
+            this.panel1.TabIndex = 0;
+            // 
+            // minButton
+            // 
+            this.minButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.minButton.Image = global::_9ball.Properties.Resources.dash1;
+            this.minButton.Location = new System.Drawing.Point(617, 0);
+            this.minButton.Name = "minButton";
+            this.minButton.Size = new System.Drawing.Size(30, 30);
+            this.minButton.TabIndex = 1;
+            this.minButton.TabStop = false;
+            this.minButton.Click += new System.EventHandler(this.minButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeButton.Image = global::_9ball.Properties.Resources.cross1;
+            this.closeButton.Location = new System.Drawing.Point(646, 0);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(30, 30);
+            this.closeButton.TabIndex = 1;
+            this.closeButton.TabStop = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.White;
+            this.title.Location = new System.Drawing.Point(8, 8);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(58, 16);
+            this.title.TabIndex = 1;
+            this.title.Text = "9ball";
+            // 
+            // ball
+            // 
+            this.ball.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ball.Image = global::_9ball.Properties.Resources._9ball;
+            this.ball.Location = new System.Drawing.Point(250, 150);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(200, 200);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(57, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(582, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Think about your question, that can be only \r\nanswered with either yes or no.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(179, 411);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "And then,";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // shake
+            // 
+            this.shake.BackColor = System.Drawing.Color.Black;
+            this.shake.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.shake.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.shake.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.shake.ForeColor = System.Drawing.Color.White;
+            this.shake.Location = new System.Drawing.Point(312, 391);
+            this.shake.Name = "shake";
+            this.shake.Size = new System.Drawing.Size(244, 65);
+            this.shake.TabIndex = 4;
+            this.shake.Text = "shake the ball.";
+            this.shake.UseVisualStyleBackColor = false;
+            this.shake.Click += new System.EventHandler(this.shake_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 500);
+            this.Controls.Add(this.shake);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "main";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.minButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.closeButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox minButton;
+        private System.Windows.Forms.PictureBox closeButton;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button shake;
+
+    }
+}
+
---
+++ 9ball/main.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace _9ball
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+            this.closeButton.MouseLeave += new System.EventHandler(this.closeButton_MouseLeave);
+            this.closeButton.MouseEnter += new System.EventHandler(this.closeButton_MouseEnter);
+            this.minButton.MouseLeave += new System.EventHandler(this.minButton_MouseLeave);
+            this.minButton.MouseEnter += new System.EventHandler(this.minButton_MouseEnter);
+            this.title.MouseEnter += new System.EventHandler(this.title_MouseEnter);
+            this.title.MouseLeave += new System.EventHandler(this.title_MouseLeave);
+            this.shake.MouseEnter += new System.EventHandler(this.shake_MouseEnter);
+            this.shake.MouseLeave += new System.EventHandler(this.shake_MouseLeave);
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void minButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void closeButton_MouseEnter(object sender, EventArgs e)
+        {
+            this.closeButton.Image = ((System.Drawing.Image)(Properties.Resources.cross2));
+        }
+        private void closeButton_MouseLeave(object sender, EventArgs e)
+        {
+            this.closeButton.Image = ((System.Drawing.Image)(Properties.Resources.cross1));
+        }
+        private void minButton_MouseEnter(object sender, EventArgs e)
+        {
+            this.minButton.Image = ((System.Drawing.Image)(Properties.Resources.dash2));
+        }
+        private void minButton_MouseLeave(object sender, EventArgs e)
+        {
+            this.minButton.Image = ((System.Drawing.Image)(Properties.Resources.dash1));
+        }
+        private void title_MouseEnter(object sender, EventArgs e)
+        {
+            this.title.Text = "eightball";
+        }
+        private void title_MouseLeave(object sender, EventArgs e)
+        {
+            this.title.Text = "9ball";
+        }
+        private void shake_MouseEnter(object sender, EventArgs e)
+        {
+            this.shake.ForeColor = System.Drawing.Color.Black;
+            this.shake.BackColor = System.Drawing.Color.White;
+        }
+        private void shake_MouseLeave(object sender, EventArgs e)
+        {
+            this.shake.ForeColor = System.Drawing.Color.White;
+            this.shake.BackColor = System.Drawing.Color.Black;
+        }
+
+        private void shake_Click(object sender, EventArgs e)
+        {
+            anim1(e, e);
+        }
+        private void anim1(object sender, EventArgs e)
+        {
+            ball.Location = new Point(ball.Location.X + 10, ball.Location.Y + 10);
+            Thread.Sleep(50);
+            ball.Location = new Point(ball.Location.X - 20, ball.Location.Y - 20);
+            Thread.Sleep(50);
+            ball.Location = new Point(ball.Location.X + 20, ball.Location.Y + 20);
+            Thread.Sleep(50);
+            ball.Location = new Point(ball.Location.X - 16, ball.Location.Y - 16);
+            Thread.Sleep(50);
+            ball.Location = new Point(ball.Location.X + 14, ball.Location.Y + 14);
+            Thread.Sleep(50);
+            ball.Location = new Point(ball.Location.X - 10, ball.Location.Y - 10);
+            Thread.Sleep(50);
+            ball.Location = new Point(ball.Location.X + 6, ball.Location.Y + 6);
+            Thread.Sleep(50);
+            ball.Location = new Point(ball.Location.X - 2, ball.Location.Y - 2);
+            Thread.Sleep(50);
+            ball.Location = new Point(ball.Location.X + 2, ball.Location.Y + 2);
+            Thread.Sleep(50);
+            ball.Location = new Point(ball.Location.X - 4, ball.Location.Y - 4);
+        }
+    }
+}
